d905d0fc8aadb79f2e204b0d49742b52ca3e8439
Jason <jasonhew1211@gmail.com>
2024-05-31 21:44:13 +0800

    Added tailwind class sort plugin to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
     'plugin:import/recommended',
+    'plugin:tailwindcss/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="relative container mx-auto p-6">
+    <nav className="container relative mx-auto p-6">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-20">
           <div className="hidden font-bold lg:flex">
-            <Link to="/search" className="text-black hover:text-darkBlue">
+            <Link to="/search" className="text-black hover:text-black">
               Search
             </Link>
           </div>
         </div>
-        <div className="hidden lg:flex items-center space-x-6 text-back">
-          <Link to="/login" className="hover:text-darkBlue">
+        <div className="hidden items-center space-x-6 text-black lg:flex">
+          <Link to="/login" className="">
             Login
           </Link>
           <Link
             to="/register"
-            className="px-8 py-3 font-bold rounded bg-lightGreen hover:opacity-70"
+            className="rounded px-8 py-3 pt-5 font-bold hover:opacity-70"
           >
             Signup
           </Link>
